cb51bf7c91e9056fb0f6191b441cd4be22d3c686
andrew <porcherface@gmail.com>
2022-08-22 17:41:31 +0200

    added deploy receipt

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# in order to run the test suite 
+# you will need truffle and ganache
+
+sudo npm install -g truffle
+sudo npm install -g ganache
+
+sudo npm install -g chai
+sudo npm install -g mocha
+
+
+pip3 install web3 --user
+
+
+
+
+# initialize truffle
+
+truffle init
+
+# truffle
